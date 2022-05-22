53a2b8bebb3889f7254f29ea8726517c1743f689
Documenter.jl <documenter@juliadocs.github.io>
2022-05-22 08:27:51 +0000

    build based on 34314a8

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Plastic","category":"page"},{"location":"#Plastic","page":"Home","title":"Plastic","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Plastic.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Plastic]","category":"page"}]
+}
